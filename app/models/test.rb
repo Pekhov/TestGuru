0d17fff97f35fa1c31412f1f5e82d01b422d7187
Pekhov <alexey@pekhov.com>
2018-11-28 07:52:29 +0300

    Lesson 5 fix

--- app/models/test.rb
+++ app/models/test.rb
@@ -1,5 +1,6 @@
 class Test < ApplicationRecord
   belongs_to :category
+  belongs_to :user
   has_many :questions
   has_and_belongs_to_many :users
 
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,6 @@
 class User < ApplicationRecord
   has_and_belongs_to_many :tests
+  has_many :tests
 
   def tests_by_level(level)
     Test.joins(:user_tests).where(user_tests: {user_id: id}, tests: {level: level})
