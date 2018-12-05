f111343cb71810316015aee34dcb2b3fcd1fffbe
Pekhov <alexey@pekhov.com>
2018-12-05 19:20:33 +0300

    Lesson 6 fix

--- app/models/test.rb
+++ app/models/test.rb
@@ -5,10 +5,11 @@ class Test < ApplicationRecord
   has_many :tests_users
   has_many :users, through: :tests_users
   scope :by_level, -> (level) { where(level: level) }
-  scope :simple, -> { where(level: 0..1) }
+  scope :simple, -> { by_level(0..1) }
   scope :middle, -> { where(level: 2..4) }
   scope :hard, -> { where(level: 5..Float::INFINITY) }
   validates :title, presence: true, uniqueness: true
+  validates :title, uniqueness: { scope: :level }
   validates :level, numericality: {only_integer: true, greater_than: 0}, uniqueness: true
 
   class << self
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_11_27_093351) do
+ActiveRecord::Schema.define(version: 2018_12_05_161732) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -46,6 +46,7 @@ ActiveRecord::Schema.define(version: 2018_11_27_093351) do
     t.datetime "updated_at", null: false
     t.bigint "user_id", null: false
     t.index ["category_id"], name: "index_tests_on_category_id"
+    t.index ["title", "level"], name: "index_tests_on_title_and_level", unique: true
     t.index ["user_id"], name: "index_tests_on_user_id"
   end
 
