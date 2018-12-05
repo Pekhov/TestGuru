fdc628cc67593fca6b7d46bcdd873cd0a14b3a8e
Pekhov <alexey@pekhov.com>
2018-12-05 09:45:56 +0300

    Lesson 6

--- app/models/test.rb
+++ app/models/test.rb
@@ -4,6 +4,12 @@ class Test < ApplicationRecord
   has_many :questions
   has_many :tests_users
   has_many :users, through: :tests_users
+  scope :by_level, -> (level) { where(level: level) }
+  scope :simple, -> { where(level: 0..1) }
+  scope :middle, -> { where(level: 2..4) }
+  scope :hard, -> { where(level: 5..Float::INFINITY) }
+  validates :title, presence: true, uniqueness: true
+  validates :level, numericality: {only_integer: true, greater_than: 0}, uniqueness: true
 
   class << self
     def tests_in_category(category)
--- app/models/user.rb
+++ app/models/user.rb
@@ -4,6 +4,6 @@ class User < ApplicationRecord
   has_many :my_created_tests, class_name: "Test"
 
   def tests_by_level(level)
-    Test.joins(:user_tests).where(user_tests: {user_id: id}, tests: {level: level})
+    tests.by_level(level)
   end
 end
