2d3e75c483d445f35618418b45acf0d960a9ab2b
Pekhov <alexey@pekhov.com>
2019-01-10 15:37:01 +0300

    Lesson 7 fix

--- app/controllers/questions_controller.rb
+++ app/controllers/questions_controller.rb
@@ -1,40 +1,50 @@
 class QuestionsController < ApplicationController
-  before_action :find_question, only: :destroy
+  before_action :find_question, only: %i(show destroy)
+  before_action :find_test, only: %i(index create)
   rescue_from ActiveRecord::RecordNotFound, with: :rescue_with_question_not_found
 
   def index
-    test = Test.find(params[:test_id])
-    render plain: test.questions.pluck(:body)
+    render plain: @test.questions.pluck(:body)
   end
 
   def show
-    render plain: Question.find(params[:id]).body
+    render plain: @question.body
   end
 
   def new
   end
 
   def create
-    question = Question.create(body: params[:question][:body], test_id: params[:test_id])
-    render plain: question.inspect
+    question = @test.questions.create(question_params)
+    if question.errors.any?
+      render plain: question.errors
+    else
+      render plain: question.inspect
+    end
   end
 
   def destroy
     @question.destroy
+    @question = nil
     render plain: "Destroy question with id=#{params[:id]}"
   end
 
   private
 
   def question_params
-    params.require(:question).permit(:body, :test_id)
+    params.require(:question).permit(:body)
   end
 
   def find_question
     @question = Question.find(params[:id])
   end
 
+  def find_test
+    @test = Test.find(params[:test_id])
+  end
+
   def rescue_with_question_not_found
-    render plain: 'Question not found'
+    response.status = 404
+    response.body = 'Question not found'
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,6 @@ Rails.application.routes.draw do
   resources :tests do
     resources :questions, shallow: true
   end
-  get '/questions/:id/destroy', to: 'questions#destroy'
+  #get '/questions/:id/destroy', to: 'questions#destroy'
 
 end
