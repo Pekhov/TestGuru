0eb3b17fc044777c43f8d254e40351353325fc6a
Pekhov <alexey@pekhov.com>
2019-01-23 13:50:01 +0300

    Lesson 9 Homework

--- app/models/tests_passage.rb
+++ app/models/tests_passage.rb
@@ -4,6 +4,7 @@ class TestsPassage < ApplicationRecord
   belongs_to :current_question, class_name: 'Question', optional: true
 
   before_validation :before_validation_set_first_question, on: :create
+  before_update :before_update_set_next_question
 
   def completed?
     current_question.nil?
@@ -13,8 +14,15 @@ class TestsPassage < ApplicationRecord
     if correct_answer?(answer_ids)
       self.correct_questions +=1
     end
-    self.current_question = next_question
     save!
+  end
+
+  def percentage_of_correct_answers
+    correct_questions ? correct_questions.to_f/test.questions.count.to_f*100 : 0
+  end
+
+  def question_num
+    test.questions.order(id: :asc).find_index(current_question) + 1
   end
 
 
@@ -26,14 +34,15 @@ class TestsPassage < ApplicationRecord
 
   def correct_answer?(answer_ids)
     correct_answers_count = correct_answers.count
-    (correct_answers_count == correct_answers.where(id: answer_ids).count) && correct_answers_count == answer_ids.count
+    correct_user_answers_count = correct_answers.where(id: answer_ids).count
+    (correct_answers_count == correct_user_answers_count) && correct_answers_count == answer_ids.count
   end
 
   def correct_answers
     current_question.answers.correct
   end
 
-  def next_question
-    test.questions.order(:id).where('id > ?', current_question.id).first
+  def before_update_set_next_question
+    self.current_question = test.questions.order(:id).where('id > ?', current_question.id).first
   end
 end
