5996769289e5fb4e5e201526a7a7cb794b2b7e57
Pekhov <alexey@pekhov.com>
2019-01-24 08:19:51 +0300

    Lesson 9 Homework fix 1

--- app/helpers/tests_passages_helper.rb
+++ app/helpers/tests_passages_helper.rb
@@ -1,2 +1,23 @@
 module TestsPassagesHelper
+  PASS_VALUE = 85.freeze
+
+  def show_result
+    result = @tests_passages.test_result
+    html = if PASS_VALUE >= 85
+      <<-EOF
+      <h2>Test passed!</h2>
+  <p>
+    Your result is <font color="green">#{result}%</font> of true answers
+  </p>
+      EOF
+    else
+      <<-EOF
+      <h2>Test was failed:(</h2>
+  <p>
+    Your result is <font color="red">#{result}%</font> of true answers
+  </p>
+      EOF
+    end
+    html.html_safe
+  end
 end
--- app/models/tests_passage.rb
+++ app/models/tests_passage.rb
@@ -4,7 +4,7 @@ class TestsPassage < ApplicationRecord
   belongs_to :current_question, class_name: 'Question', optional: true
 
   before_validation :before_validation_set_first_question, on: :create
-  before_update :before_update_set_next_question
+  before_validation :before_validation_set_next_question, on: :update
 
   def completed?
     current_question.nil?
@@ -17,19 +17,22 @@ class TestsPassage < ApplicationRecord
     save!
   end
 
-  def percentage_of_correct_answers
-    correct_questions ? correct_questions.to_f/test.questions.count.to_f*100 : 0
+  def test_result
+    correct_questions ? correct_questions.to_f / test.questions.count.to_f * 100 : 0
   end
 
   def question_num
     test.questions.order(id: :asc).find_index(current_question) + 1
   end
 
-
   private
 
   def before_validation_set_first_question
     self.current_question = test.questions.first if test.present?
+  end
+
+  def before_validation_set_next_question
+    self.current_question = test.questions.order(:id).where('id > ?', current_question.id).first
   end
 
   def correct_answer?(answer_ids)
@@ -40,9 +43,5 @@ class TestsPassage < ApplicationRecord
 
   def correct_answers
     current_question.answers.correct
-  end
-
-  def before_update_set_next_question
-    self.current_question = test.questions.order(:id).where('id > ?', current_question.id).first
   end
 end
