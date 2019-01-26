315f3a41f81b83f8ee8f9c1a07c66975de527ad9
Pekhov <alexey@pekhov.com>
2019-01-26 15:05:10 +0300

    Lesson 10 Homework fix

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_01_25_061208) do
+ActiveRecord::Schema.define(version: 2019_01_26_115711) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -68,6 +68,7 @@ ActiveRecord::Schema.define(version: 2019_01_25_061208) do
     t.datetime "updated_at", null: false
     t.string "password_digest"
     t.string "email"
+    t.index ["email"], name: "index_users_on_email", unique: true
   end
 
   add_foreign_key "answers", "questions"
