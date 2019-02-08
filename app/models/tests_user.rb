73a9a9ab528819055aa945df302063f0549e8709
Pekhov <alexey@pekhov.com>
2019-02-08 11:29:55 +0300

    Lesson 17. JRuby path fix 2

--- app/models/tests_user.rb
+++ app/models/tests_user.rb
@@ -1,5 +1,5 @@
 class TestsUser < ApplicationRecord
   belongs_to :user
   belongs_to :test
-  belongs_to :current_question, class_name: Question
+  belongs_to :current_question, class_name: 'Question'
 end
