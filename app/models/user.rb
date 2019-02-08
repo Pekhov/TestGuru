bf3443096e917b67ed57a3d90d88f44f09e24e00
Pekhov <alexey@pekhov.com>
2019-02-08 14:36:54 +0300

    Lesson 17. fix 4

--- app/models/user.rb
+++ app/models/user.rb
@@ -8,6 +8,7 @@ class User < ApplicationRecord
   has_many :tests, through: :tests_passages
   has_many :my_created_tests, class_name: "Test"
   has_many :gists
+  has_many :feedbacks
   VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
   validates :email, presence: true, format: { with: VALID_EMAIL_REGEX }, uniqueness: { case_sensitive: true }
 
--- config/environments/production.rb
+++ config/environments/production.rb
@@ -64,7 +64,7 @@ Rails.application.configure do
   # config.active_job.queue_name_prefix = "TestGuru_#{Rails.env}"
 
   config.action_mailer.perform_caching = false
-  config.action_mailer.default_url_options = { host: 'floating-bastion-96238.herokuapp.com' }
+  config.action_mailer.default_url_options = { host: 'test-guru777.herokuapp.com' }
   config.action_mailer.delivery_method = :smtp
   config.action_mailer.smtp_settings = {
     address: 'smtp.gmail.com',
